83d782dd42e7c6948ba33d36e9c687f48249efd2
hrfmmymt <hrfmmymt@gmail.com>
2016-10-21 19:09:32 +0900

    root component.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello, react-min world!</h1>, document.querySelector(".container"));
+class Hello extends React.Component {
+  render() {
+    return <h1>Hello, react-min world!</h1>;
+  }
+}
+
+ReactDOM.render(
+  <Hello />,
+  document.querySelector(".container")
+);
